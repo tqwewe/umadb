87bfa587ae4151907625449a294842f8c5b48132
johnbywater <john.bywater@appropriatesoftware.net>
2025-11-17 22:51:17 +0000

    Adjusted build to use protoc from Rust crate protobuf-src.

--- crates/proto/build.rs
+++ crates/proto/build.rs
@@ -1,4 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    unsafe {
+        std::env::set_var("PROTOC", protobuf_src::protoc());
+    }
     tonic_prost_build::compile_protos("./umadb.proto")?;
     Ok(())
 }
